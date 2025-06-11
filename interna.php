72931fd1433d5a491fd3989c3a32a977487be0a7
Matheus Alves <54443770+matthaiosalves@users.noreply.github.com>
2025-06-11 18:45:11 -0300

    Initial Commit

---
+++ interna.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Template Name: Interna
+*/
+get_header();
+?>
+<style>
+  img {
+    max-width: 100%;
+    width: 100%;
+  }
+</style>
+<?php get_template_part('template-parts/banner'); ?>
+
+<section class="sobreNos" style="margin-top:70px;margin-bottom:70px;">
+  <div class="container-fluid">
+    <div class="row">
+      <h3 class="title"><?php echo esc_html(get_the_title()); ?></h3>
+
+      <h2 class="subtitle"><?php the_field('subtitulo'); ?></h2>
+
+      <div class="boxContent">
+        <?php the_content(); ?>
+      </div>
+    </div>
+
+    <?php
+    $url = get_field('botao_whatszapp');
+    if (!empty($url)) : ?>
+      <div class="row justify-content-center align-items-center mt-4 mb-5">
+        <a href="<?php echo esc_url($url); ?>" class="btn btn-zap" target="_blank">
+          Entrar no grupo
+          <img src="<?php echo get_template_directory_uri(); ?>/img/whatsapp.svg" alt="Ícone do WhatsApp" class="img-fluid imgZap" width="45" height="45" loading="lazy">
+        </a>
+      </div>
+    <?php endif; ?>
+
+    <?php
+    $imagem_destaque = get_field('imagem_destaque');
+    if (!empty($imagem_destaque)) : ?>
+      <img src="<?php echo esc_url($imagem_destaque); ?>" alt="" class="img-fluid" width="1170" height="356" loading="lazy">
+    <?php endif; ?>
+
+  </div>
+</section>
+
+<?php get_template_part('template-parts/contato'); ?>
+
+<?php
+get_footer();
--- template-parts/equipe.php
+++ template-parts/equipe.php
@@ -1,13 +1,14 @@
 <?php
 $equipe = get_field('equipe');
 ?>
-<section class="conheca-equipe" id="conheca-equipe">
+<section class="conheca-equipe" id="page" style="background-color: #F7F9FE !important;">
   <div class="container-fluid">
 
     <div class="row">
       <div class="boxTitleAndDescription">
-        <h2 class="title"><?php the_field('titulo'); ?></h2>
-        <p class="description"><?php the_field('subtitulo'); ?></p>
+        <h2 class="title" style="font-size:18px;font-family:'Lato';"><?php the_field('titulo'); ?></h2>
+        <p class="subtitle"><?php the_field('subtitulo'); ?></p>
+        <p class="description" style="font-size:18px;font-family:'Lato';"><?php the_field('descricao'); ?></p>
       </div>
     </div>
     <hr />
@@ -20,10 +21,10 @@ $equipe = get_field('equipe');
           </figure>
 
           <div class="boxTitleAndDescription">
-            <h2 class="title"><?php echo $item['nome']; ?></h2>
-            <p><?php echo $item['posicao']; ?></p>
+            <h2 class="title" style="font-size:25px;font-family:'Lato';font-weight:bold;"><?php echo $item['nome']; ?></h2>
+            <p style="font-size:21px;font-family:'Lato';font-weight:500;"><?php echo $item['posicao']; ?></p>
             <hr />
-            <p class="description"><?php echo $item['descricao']; ?></p>
+            <p class="description" style="font-size:17px;font-family:'Lato';"><?php echo $item['descricao']; ?></p>
           </div>
         </div>
       <?php endforeach; ?>
